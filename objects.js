70255dade6d2b6b5d2dc28c457de14e7b254d0bc
akki4169@gmail.com Kulkarni <akki4169@gmail.com>
2018-04-22 18:52:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,6 @@ var playlist={shamak:"mohabbat kar le kar le"};
 function updatePlaylist(playlist,name,value){
   return Object.assign(playlist,{[name]:value})
 }
+function removeFromPlaylist(){
+  
+}
