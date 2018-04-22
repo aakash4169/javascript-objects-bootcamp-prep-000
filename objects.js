dc67a47c664b00b039219286fc7a56f70f480e60
akki4169@gmail.com Kulkarni <akki4169@gmail.com>
2018-04-22 18:56:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlist,name,value){
   return Object.assign(playlist,{[name]:value})
 }
 function removeFromPlaylist(playlist,name){
-  delete playlist.name;
+  delete playlist.[name];
   return playlist;
 }
